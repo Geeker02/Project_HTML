a5fb7a8fedd04e65b3e89ce8e598e79f810e75cb
Geeker02 <jaydeepjogdand@gmail.com>
2023-10-08 18:07:21 +0530

    add files

---
+++ gym/Project1/script.js
@@ -0,0 +1,9 @@
+// Add items to the cart
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        // Add the item to the cart
+        // ...
+    });
+});
